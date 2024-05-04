4146c19318c3db9c4519a05d84a2039e08a7a282
Sardine184 <kuuhaku611@gmail.com>
2024-05-04 23:58:56 +0900

    「フルーツのプレファブとプレファブバリアント」 成功例)さくらんぼといちごとがぶつかっても消えない

--- Assets/Scripts/Fruits.cs
+++ Assets/Scripts/Fruits.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FRUITS_TYPE
+{
+    さくらんぼ = 1,
+    いちご,
+    ぶどう,
+    オレンジ,
+    かき,
+    りんご,
+    なし,
+    もも,
+    パイナップル,
+    メロン,
+    すいか,
+}
+
 public class Fruits : MonoBehaviour
 {
+    public FRUITS_TYPE fruitsType;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.TryGetComponent(out Fruits otherFruits))
         {
-            Destroy(gameObject);
+            //相手のフルーツタイプと自分のフルーツタイプとが同じならば削除を行う
+            //ex)いちご,いちご -> 消える　いちご,さくらんぼ -> 消えない
+            if(otherFruits.fruitsType == fruitsType)
+            {
+                Destroy(gameObject);
+            }
         }
     }
     
